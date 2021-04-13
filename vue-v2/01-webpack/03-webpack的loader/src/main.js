c4a204547dda88c5de1a39c9bc59342e5e19f102
code-tbd <ukmukk@163.com>
2021-04-14 01:34:22 +0800

    main

--- vue-v2/01-webpack/03-webpack的loader/src/main.js
+++ vue-v2/01-webpack/03-webpack的loader/src/main.js
@@ -1,11 +1,11 @@
 // commonJS导入
-const {sum,mul} = require('./mathUtils')
+const {sum,mul} = require('./js/mathUtils')
 
 console.log(sum(1,2));
 console.log(mul(2,3));
 
 // ES6导入
-import * as info from './info'
+import * as info from './js/info'
 console.log(info.name);
 console.log(info.height);
 console.log(info.age);
