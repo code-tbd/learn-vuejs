062119c357233e870c4fe61bdbe8bfd80de9b5c0
code-tbd <ukmukk@163.com>
2021-04-14 22:11:46 +0800

    main

--- vue-v2/01-webpack/03-webpack的loader/webpack.config.js
+++ vue-v2/01-webpack/03-webpack的loader/webpack.config.js
@@ -6,7 +6,10 @@ module.exports = {
   output: {
     // 这里的__dirname通过node获得,再使用resolve拼接路径
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+
+    // publicPath 任何涉及到的url都会在前面添加指定的路径
+    publicPath: 'dist/'
   },
   module: {
     rules: [
@@ -31,12 +34,17 @@ module.exports = {
       {
         // 图片的webpack配置
 
-        test: /\.(png|jpg|gif)$/i,
+        test: /\.(png|jpg|gif|jpeg)$/i,
         use: [
           {
             loader: 'url-loader',
             options: {
+              // 当图片大小小于limit时，会将图片编译成base64字符串形式
+              // 当图片大于limit时，需要使用file-loader模块进行加载
               limit: 8192,
+
+              // [name]-原名 [hash:n]n位哈希 [ext]扩展名
+              name: 'img/[name].[hash:8].[ext]'
             },
           },
         ],
